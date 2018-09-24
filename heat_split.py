4416bec1e9f8a8be851dac2ffd596e2201839654
Nick Williams <nick.williams@striderhq.local>
2018-09-24 11:32:18 -0600

    playing around with grouping algos

--- heat_split.py
+++ heat_split.py
@@ -12,14 +12,42 @@ def test_for_range():
         avg_size = splits[1]
         #last = splits[2]
         histo = '   ' + str(parts) + '   :: '
-        for x in range(0, int(numRaces-1)):
+        for x in range(0, int(numRaces)):
             histo += ' ' + str(avg_size)
-        histo += ' ' + str((avg_size*numRaces)-parts)
-        histo += '     races = ' + str(numRaces) + '  capacity: ' + str(avg_size * numRaces)
-        '''
-        histo += '   raceXavg_race_size = ' + str(numRaces * avg_size)
-        '''
+        
+        histo += '   number of races = ' + str(numRaces)
+        
         print(histo)
+
+def test_for_range_two():
+    upper = 1000
+    lower = 16
+    gates = 16
+    for ix in range(lower,upper):
+        race_list = split_races_two(gates, ix)
+        print(race_list)
+
+def split_races_two(g, p):
+    nr = 0
+    race_list = []
+    if(p%g == 0):
+        nr = p//g
+        for x in range(0, nr):
+            race_list.append(g)
+        return race_list
+    else:
+        nr = p//g + 1
+        short_races = g - (p%g)
+        size_correct = 1
+        if(short_races > nr):
+            size_correct 
+        for x in range(0, nr):
+            if (x < short_races):
+                race_list.append(g-1)
+            else:
+                race_list.append(g)
+        return race_list
+
 
 def get_splits_(g, p):
     nr = 0
@@ -52,7 +80,7 @@ def get_splits_(g, p):
         nr += 1
     return nr, race_size
 
-test_for_range()
+#test_for_range_two()
 
 
 
