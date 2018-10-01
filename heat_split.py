3d8372bf0ca68665f243340e625c79d2fd579f4b
Nick Williams <nick.williams@striderhq.local>
2018-10-01 12:32:56 -0600

    logic to split heat races down into main races

--- heat_split.py
+++ heat_split.py
@@ -6,15 +6,16 @@ def test_for_range():
     upper = 321
     lower = 10
     gates = 8
-    with open("%d_gate_heats.csv" %(gates), 'w') as f:
+    with open("%d_gate_mains.csv" %(gates), 'w') as f:
         for parts in range(lower,upper):
             firstLine = "%d racers \n" %(parts)
             f.write(firstLine)
             splits = get_splits_(gates, parts)
             heats = create_race_list(splits[0], splits[1], parts)
+            mains = split_heat_to_finals(heats, parts)
+            print(heats)
             print(mains)
-            for i in heats:
-                main = 
+            for i in mains:
                 nextLine = "%d ," %(i)
                 f.write(nextLine)
             f.write("\n")
@@ -30,12 +31,13 @@ def test_for_range():
         
         print(histo)
         '''
+    return
 
 
 def get_splits_(g, p):
     nr = 0
     if (p//g < g/2):
-        g = g//2
+        g = g- (g//3)
         nr = p//g
         race_size = g
     else:
@@ -76,29 +78,31 @@ def create_race_list(numRaces, rSize, racers):
 
 def split_heat_to_finals(race_list, np):
     if (np > 40):
-        return split_heats_over_40(race_list, np)
+        return split_heats_over(race_list, np)
     else:
         return split_heats_under(race_list, np)
 
-
+# under forty racers, its not big deal if we have num_mains = num_heats
 def split_heats_under(race_list, np):
     mains = []
-    mains[0] = len(race_list)
-    sub_main_size = (np - len(race_list))/(len(race_list) - 1)
-    left_overs = sub_main_size + ((np-len(race_list)) %(len(race_list) - 1)
-    for x in range(1, len(race_list)-1):
-        mains[x] = sub_main_size
-    mains[-1] += leftovers
+    mains.append(len(race_list))
+    sub_main_size = (np - len(race_list))//(len(race_list) - 1)
+    left_overs = sub_main_size + ((np-len(race_list)) %(len(race_list) - 1))
+    for x in range(1, len(race_list)):
+        mains.append(sub_main_size)
+    mains.append(left_overs)
+    return mains
 
+#over forty races so we will reduce from num_heats    
 def split_heats_over(race_list, np):
     mains = []
-    mains[0] = len(race_list)
-    less_mains = len(race_list) - (len(race_list)/3)
-    main_size = np/less_mains
+    mains.append(len(race_list))
+    less_mains = len(race_list) - int(len(race_list)//3)
+    main_size = np//less_mains
     for x in range(1, less_mains):
-        mains[x] = main_size
-    mains[-1] += np%less_mains
-
+        mains.append(main_size)
+    mains.append(main_size + np%less_mains)
+    return mains
 
 
 
